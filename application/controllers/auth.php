cd5172a87ee874665419d4ab7ce08bdc1a0de816
Jayson <jaysonsoria517@gmail.com>
2021-10-10 18:08:21 +0800

    add

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -1,28 +1,15 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Home extends CI_Controller {
+class Auth extends CI_Controller {
 
 	public function __construct()
 	{
 		parent::__construct();
-		// if($this->session->userdata()) {
-
-		// } else {
-
-		// }
 	}
 
 	public function index()
 	{
 		$this->load->view('main_view');
 	}
-
-	public function logout()
-	{
-		// $this->session->sess_destroy();
-		// redirect('/','refresh');
-	}
-
-
 }
